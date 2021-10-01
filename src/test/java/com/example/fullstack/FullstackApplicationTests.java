4d1f12a8bd5eece3cd33fb5c3c54aa4f239cae49
RobertSitek16 <robert.sitek16@gmail.com>
2021-10-01 13:41:11 +0200

    Make test success

--- src/test/java/com/example/fullstack/FullstackApplicationTests.java
+++ src/test/java/com/example/fullstack/FullstackApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.fullstack;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class FullstackApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("The test will fails!");
 	}
 
 }
